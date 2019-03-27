27aac03cbd8da2a0aaeded14de0ebc5934d4acfa
zbrasseaux <zbrasseaux97@gmail.com>
2019-03-27 14:34:06 -0500

    implemented basic vigenere cipher, definately needs work

--- assign2/vigenere.py
+++ assign2/vigenere.py
@@ -1,16 +1,54 @@
 #!/usr/bin/env python3
 import sys
 
-def cipher(key, inString):
-	print(key, inString)
+alphabet_length = 26
+
+def strToAscii(inArr):
+	temp = []
+	for char in inArr:
+		temp.append(ord(char))
+
+	return temp
+
+def asciiToStr(inArr):
+	temp = ""
+
+	for char in inArr:
+		temp = temp + chr(char)
+
+	return temp
+
+def encrypt(key, inString):
+	finArr = []
+
+	key = strToAscii(list(key))
+	inString = strToAscii(list(inString))
+
+	for i in range(0,len(inString)):
+		kval = i%len(key)
+		finArr.append((inString[i] + key[kval]) % alphabet_length)
+
+	print(asciiToStr(finArr))
+
+def decrypt(key, inString):
+	finArr = []
+
+	key = strToAscii(list(key))
+	inString = strToAscii(list(inString))
+
+	for i in range(0,len(inString)):
+		kval = i%len(key)
+		finArr.append((inString[i] - key[kval]) % alphabet_length)
+
+	print(asciiToStr(finArr))
 
 if (sys.argv[-2] == '-e' or sys.argv[-2] == '--encrypt'):
 	inString = input()
-	cipher(sys.argv[-1], inString)
+	encrypt(sys.argv[-1], inString)
 
 elif (sys.argv[-2] == '-d' or sys.argv[-2] == '--decrypt'):
 	inString = input()
-	cipher(sys.argv[-1], inString)
+	decrypt(sys.argv[-1], inString)
 
 else:
 	print("Invalid option, please try again.")

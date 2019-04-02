a8b025419b69c33e188f45eb423cad11d71dfefe
Trenton <38508613+KiMage44@users.noreply.github.com>
2019-04-02 15:15:20 -0500

    Update covert.py

--- assign3/covert.py
+++ assign3/covert.py
@@ -7,7 +7,8 @@
 
 from ftplib import FTP
 dirList = []
-METHOD = 7
+METHOD = 10
+PRINT = False
 
 #Method to convert incoming data into a binary format
 def conToBinary(filePermList):
@@ -19,10 +20,11 @@ def conToBinary(filePermList):
                 tempHolder = tempHolder + "0"
             else:
                 tempHolder = tempHolder + "1"
-        convertBinary.append(tempHolder)    
-    #print " "
-    #for f in range(len(convertBinary)):
-        #print convertBinary[f]  
+        convertBinary.append(tempHolder)
+    if(PRINT == True):
+        print "\nBinary Format:"
+        for f in range(len(convertBinary)):
+            print convertBinary[f]  
     return convertBinary
 
 #Method to convert incoming binary data into it's integer form
@@ -30,9 +32,10 @@ def conToInt(fullBinary):
     binaryInt = []
     for l in range(len(fullBinary)):
         binaryInt.append(int(fullBinary[l],2))
-    #print ""
-    #for y in range(len(binaryInt)):
-        #print binaryInt[y]
+    if(PRINT == True):
+        print "\nInteger Sum Format:"
+        for y in range(len(binaryInt)):
+            print binaryInt[y]
     return binaryInt
 
 #Converts integer number into corresponding ASCII character
@@ -40,7 +43,7 @@ def conToASCII(binaryInt):
     ASCIIrep = [] 
     for u in range(len(binaryInt)):
         ASCIIrep.append(chr(binaryInt[u]))
-    print ""
+    print "\nASCII translation:"
     print ASCIIrep
     return ASCIIrep
 
@@ -70,9 +73,10 @@ if(METHOD == 7):
                 filePermList[i] = "ERRONEOUS"
                 errorCounter += 1
                 break
-    #print ""
-    #for l in range(len(filePermList)):
-        #print filePermList[l]
+    if(PRINT == True):
+        print ""
+        for l in range(len(filePermList)):
+            print filePermList[l]
             
 ## Removes the files marked to be ignored  
     for t in range(errorCounter):
@@ -81,10 +85,11 @@ if(METHOD == 7):
     permList = []
     for o in range(len(filePermList)):
         permList.append(filePermList[o][3:10])
-#Prints a list of file permissions now formatted to last 7 bits     
-    #print ""
-    #for y in range(len(permList)):
-        #print permList[y]
+#Prints a list of file permissions now formatted to last 7 bits
+    if(PRINT == True):
+        print ""
+        for y in range(len(permList)):
+            print permList[y]
     fullBinary = conToBinary(permList)  #Runs method to convert list to binary  
     binaryInt = conToInt(fullBinary)    #Runs method to convert binary list to integer
     ASCII = conToASCII(binaryInt)   #Runs method to convert integers to ASCII characters
@@ -95,28 +100,44 @@ elif(METHOD == 10):
     longString = []
     for r in range(len(filePermList)):
         longString.append(filePermList[r])
-    #print ""
-    #print "String: {}".format(longString)
+    if(PRINT == True):
+        print ""
+        print "String: {}".format(longString)
 
 #Finds if the length is easily broken up into groups of 7, adds zeroes to the last one if not
     lengthString = len(longString)
     modVal = lengthString % 7
-    #print("Mod Value: {}".format(modVal))
-    #print("Modulus is not 0, adding {} zeros...".format(modVal))
+    if(PRINT == True):
+        print("Mod Value: {}".format(modVal))
+        
     if(modVal != 0):
-        addValue = 7-modVal
-        longString[-1] = "-"*addValue + longString[-1]
-        #print("\n New String: {}".format(longString))
+        addValue = 8-modVal
+        if(PRINT == True):
+            print("Modulus is not 0, adding {} to final sequence...".format(addValue))
+            print longString[-1]
+            
+        longString[-1] = ("-"*addValue) + longString[-1]
+        
+        if(PRINT == True):
+            print longString[-1]
+            print("\nNew String (still in list format): {}".format(longString))
+            
     newString = ""
     for n in range(len(longString)):
         newString += longString[n]
-    #print newString
+            
+    if(PRINT == True):
+        print "New String in comprehensive format: {}".format(newString)
+        print "Length%7: {}".format(len(newString)%7)
 
     bit7Convert = []
     for v in range(len(newString)/7):
         bit7Convert.append(newString[(7*v):(7+(7*v))])
-    #for b in range(len(bit7Convert)):
-        #print bit7Convert[b]
+    if(PRINT == True):
+        print "\nConverted to bit7 format: "
+        for b in range(len(bit7Convert)):
+            print bit7Convert[b]
+            
 #Runs method to convert list to binary    
     fullBinary = conToBinary(bit7Convert)
 #Runs method to convert binary list to integer
@@ -124,7 +145,7 @@ elif(METHOD == 10):
 #Runs method to convert integers to ASCII characters
     ASCII = conToASCII(binaryInt)
 else:
-    print "Please enter a 7 or 10 into the METHOD variable, other values will not be accepted.
+    print "Please enter a 7 or 10 into the METHOD variable, other values will not be accepted."
     exit(0);
         
     

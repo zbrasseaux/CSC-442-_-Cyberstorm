88ba29120d01feccf87a5015e0425be2b1f49272
Addison <addisonabrcrmb5@gmail.com>
2019-04-15 20:46:44 -0500

    added img, key2 in xor.py now

--- assign6/xor.py
+++ assign6/xor.py
@@ -14,7 +14,7 @@ for i in pipedBinary:
 #     byte = byteList1.read(1)
 
 byteList2 = []
-with open("key", "rb") as f:
+with open("key2", "rb") as f:
     byte = f.read(1)
     while byte != "":
         byteList2.append(byte)

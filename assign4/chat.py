9c079fc6676f4c298599b1594ea56e56e39b921e
zbrasseaux <zbrasseaux97@gmail.com>
2019-04-18 12:19:39 -0500

    updates

--- assign4/chat.py
+++ assign4/chat.py
@@ -1,19 +1,8 @@
-'''
-Team Celtics
-
-Link to private GitHub:
-https://github.com/zbrasseaux/CSC-442-_-Cyberstorm
-
-If you need access to it, please email any of out members.
-'''
-
 import socket
 from time import time
 import sys
 import binascii
 
-
-# For best results use wired connection
 
 address = 'www.jeangourd.com'
 port = 31337
@@ -24,11 +13,11 @@ covert = ""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((address, port))
 
-data = s.recv(4096)
+data = ""
+
+# print(data)
 
 while (data.rstrip("\n") != "EOF"):
-	sys.stdout.write(data)
-	sys.stdout.flush()
 
 	t0 = time()
 	data = s.recv(4096)
@@ -36,20 +25,25 @@ while (data.rstrip("\n") != "EOF"):
 
 	delta = round(t1 - t0, 3)
 
-	if (delta >= .095): # can be changed in the future to improve accuracy
+	# print(delta)
+
+	if (delta >= .08):
 		covert_bin += "1"
 	else:
 		covert_bin += "0"
+
+	sys.stdout.write(data)
+	sys.stdout.flush()
 
 s.close()
 
 i = 0
 while (i < len(covert_bin)):
-
 	# process one byte at a time
 	b = covert_bin[i:i + 8]
 	# convert it to ASCII
 	n = int("0b{}".format(b), 2)
+
 
 	try:
 		covert += binascii.unhexlify("{0:x}".format(n))
@@ -60,3 +54,4 @@ while (i < len(covert_bin)):
 	i += 8
 
 print(covert.rstrip("EOF?"))
+print(covert_bin)

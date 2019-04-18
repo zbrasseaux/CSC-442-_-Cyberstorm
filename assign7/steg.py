d979c4f07d1754525d4c222ffc330bc2fe252330
zbrasseaux <zbrasseaux97@gmail.com>
2019-04-18 12:20:51 -0500

    PEP standards

--- assign7/steg.py
+++ assign7/steg.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 
 import sys
+from PIL import Image
 
-# interval defaults to 1
-interval = 1
+####### Error Codes #######
+# 0 : Exited with no issues
+# 1 : Invalid flag
+# 2 : No MODE set
 
-# help fxn that gives the usage and options
+# global var declarations
+
+# INTERVAL defaults to 1
+INTERVAL = 1
+
+FLAG = ''
+MODE = ''
+METHOD = ''
+OFFSET = ''
+WRAPPER_FILE = ''
+HIDDEN_FILE = ''
+
 def help():
+	'''help fxn that gives the usage and options'''
 	sys.stdout.write\
 	("Usage: 'python steg.py -(bB) -(sr) -o<val> [-i<val>] -w<val> [-h<val>]'\n\n")
 	sys.stdout.write("\t-b\t  Use the bit method\n")
@@ -20,51 +35,78 @@ def help():
 	sys.stdout.write("\n\t--help\t  Display this message\n")
 	exit(0)
 
-# some code to test that the different flags work
 def test():
-	print('Mode : ' + str(mode))
-	print('Method : ' + str(method))
-	print('Interval : ' + str(interval))
-	print('Offset : ' + str(offset))
-	print('Hidden File : ' + hiddenFile)
-	print('Wrapper : ' + wrapper)
+	'''some code to test that the different flags work'''
+	print('Mode : ' + str(MODE))
+	print('Method : ' + str(METHOD))
+	print('Interval : ' + str(INTERVAL))
+	print('Offset : ' + str(OFFSET))
+	print('Hidden File : ' + HIDDEN_FILE)
+	print('Wrapper : ' + WRAPPER_FILE)
 
-# parser to set/change different values
-for i in sys.argv[1:]:
-	temp = list(i)
+def store():
+	return 0
 
-	# help menu
-	if (i == '--help'):
-		help()
-	# store mode
-	elif (temp[1] == 's'):
-		mode = 0
-	# retrieve mode
-	elif (temp[1] == 'r'):
-		mode = 1
-	# bit method
-	elif (temp[1] == 'b'):
-		method =0
-	# byte method
-	elif (temp[1] == 'B'):
-		method = 1
-	# offset value
-	elif (temp[1] == 'o'):
-		offset = int(''.join(temp[2:]))
-	# change default inverval value
-	elif (temp[1] == 'i'):
-		interval = int(''.join(temp[2:]))
-	# declare hidden file
-	elif (temp[1] == 'h'):
-		hiddenFile = ''.join(temp[2:])
-	# declare wrapper file
-	elif (temp[1] == 'w'):
-		wrapper = ''.join(temp[2:])
+def retrieve():
+	return 1
+	
 
-	# catch-all error statement for invalid options
-	else:
-		sys.stderr.write("Invalid option : " + i + \
-			", please try again, or use --help for more options.\n")
-		exit(1)
+##### Main Program #####
 
-test()
+def main():
+	'''parser to set/change different values'''
+	for i in sys.argv[1:]:
+		temp = list(i)
+
+		FLAG = ''.join(temp[0:2])
+
+		# help menu
+		if (i == '--help'):
+			help()
+		# store MODE
+		elif (FLAG == '-s'):
+			MODE = 0
+		# retrieve MODE
+		elif (FLAG == '-r'):
+			MODE = 1
+		# bit METHOD
+		elif (FLAG == '-b'):
+			METHOD =0
+		# byte METHOD
+		elif (FLAG == '-B'):
+			METHOD = 1
+		# OFFSET value
+		elif (FLAG == '-o'):
+			OFFSET = int(''.join(temp[2:]))
+		# change default inverval value
+		elif (FLAG == '-i'):
+			INTERVAL = int(''.join(temp[2:]))
+		# declare hidden file
+		elif (FLAG == '-h'):
+			HIDDEN_FILE = ''.join(temp[2:])
+		# declare WRAPPER_FILE file
+		elif (FLAG == '-w'):
+			WRAPPER_FILE = ''.join(temp[2:])
+
+		# catch-all error statement for invalid options
+		else:
+			sys.stderr.write("Invalid option : " + i + \
+				", please try again, or use --help for more options.\n")
+			exit(1)
+
+	# runs retrieve or store based on the MODE
+	# includes error handling for errors that I encountered
+
+	try:
+		if (MODE == 1):
+			retrieve()
+		elif (MODE == 0):
+			store()
+	except NameError:
+		sys.stderr.write\
+		("Mode (store/retrieve) not set, please try again or see '--help' for more options.\n")
+		exit(2)
+
+main()
+# Program exits successfully
+exit(0)

b4de37b35c26583c07ab3039a6b9b089637da823
zbrasseaux <zbrasseaux97@gmail.com>
2019-04-25 21:39:34 -0500

    commented chat_client.py

--- assign4/chat_client.py
+++ assign4/chat_client.py
@@ -7,6 +7,7 @@ https://github.com/zbrasseaux/CSC-442-_-Cyberstorm
 If you need access to it, please email any of out members.
 '''
 
+# Library imports
 import socket
 from time import time
 import sys
@@ -15,32 +16,41 @@ import binascii
 
 # For best results use wired connection
 
-address = 'www.jeangourd.com'
+# address and ports
+address = 'jeangourd.com'
 port = 31337
 
+# empty strings to be filled with data
 covert_bin = ""
 covert = ""
 
+# initializing connections
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((address, port))
 
+# allows for the recieving of packets
 data = s.recv(4096)
 
+# accepts and iterated through the overt message
 while (data.rstrip("\n") != "EOF"):
 	sys.stdout.write(data)
 	sys.stdout.flush()
 
+	# calculates time between characters
 	t0 = time()
 	data = s.recv(4096)
 	t1 = time()
 
+	# rounds the time to 3 decimal places
 	delta = round(t1 - t0, 3)
 
+	# determines a one or a zero for the time
 	if (delta >= .07): # can be changed in the future to improve accuracy
 		covert_bin += "1"
 	else:
 		covert_bin += "0"
 
+# terminates the connection
 s.close()
 
 i = 0
@@ -51,6 +61,8 @@ while (i < len(covert_bin)):
 	# convert it to ASCII
 	n = int("0b{}".format(b), 2)
 
+	# attempt to convert binary to ascii chars
+	# if it fails, it uses '?'
 	try:
 		covert += binascii.unhexlify("{0:x}".format(n))
 	except TypeError:
@@ -59,4 +71,5 @@ while (i < len(covert_bin)):
 	# stop at the string "EOF"
 	i += 8
 
+# outputs the final string w/o the 'EOF?'
 print(covert.rstrip("EOF?"))
